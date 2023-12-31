5aee76ec9e0adb866956ac9ce3dcb3ae7a950786
do dung <dominhdung21082002@gmail.com>
2023-12-31 16:02:12 +0700

    layout in admin

--- src/layouts/AdminLayouts/SideBarAdmin.tsx
+++ src/layouts/AdminLayouts/SideBarAdmin.tsx
@@ -1,65 +1,169 @@
 import { ReactNode, useState } from "react";
+import { useLocation, useNavigate } from 'react-router-dom';
 import React from "react";
-import {
-  LaptopOutlined,
-  NotificationOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+import {LaptopOutlined,NotificationOutlined, UserOutlined,CarOutlined,ProfileOutlined, MoneyCollectOutlined, DashboardOutlined,} from "@ant-design/icons";
 import type { MenuProps } from "antd";
 import { Breadcrumb, Layout, Menu, theme } from "antd";
-import './main.scss';
+import "./main.scss";
+import Title from "antd/es/typography/Title";
+import RouterConfigs from "../../configs";
+import GetItem from "../../commons/helpers/functions/GetItem";
+import { log } from "console";
+type MenuItem = Required<MenuProps>['items'][number];
 
 const { Header, Content, Footer, Sider } = Layout;
 
-const items2: MenuProps["items"] = [
-  UserOutlined,
-  LaptopOutlined,
-  NotificationOutlined,
-].map((icon, index) => {
-  const key = String(index + 1);
 
-  return {
-    key: `sub${key}`,
-    icon: React.createElement(icon),
-    label: `subnav ${key}`,
 
-    children: new Array(4).fill(null).map((_, j) => {
-      const subKey = index * 4 + j + 1;
-      return {
-        key: subKey,
-        label: `option${subKey}`,
-      };
-    }),
-  };
-});
+//#region Define Menu
+const items2 = [
+  GetItem(
+    "Dashboard",
+    RouterConfigs.adminRouters.home,
+    <DashboardOutlined />,
+  ),
 
-const SideBarAdmin = ({collapsed, SetCollapsed}: {collapsed: boolean, SetCollapsed: any}) => {
+  GetItem(
+    "Quản lý tài khoản",
+    "1",
+    <UserOutlined />,
+    [
+      GetItem("Danh sách tài khoản",  RouterConfigs.adminRouters.home, ),
+      GetItem("Thống kê tài khoản",  '1-1', ),
+    ],
+  ),
+  GetItem(
+    "Quản lý công ty",
+    "2",
+    <ProfileOutlined />,
+    [
+      GetItem("Danh sách công ty",  RouterConfigs.adminRouters.manageCompany, ),
+      GetItem("Thống kê công ty",  '2-1', ),
+    ],
+  ),
+
+  GetItem(
+    "Quản lý loại xe",
+    "3",
+    <CarOutlined />,
+    [
+      GetItem("Danh sách loại xe",  RouterConfigs.adminRouters.manageBusType, ),
+      GetItem("Thống kê loại xe",  '3-1', ),
+    ],
+  ),
+
+  GetItem(
+    "Quản lý ghế",
+    "4",
+    <CarOutlined />,
+    [
+      GetItem("Danh sách loại ghế",  RouterConfigs.adminRouters.manageSeatType, ),
+      GetItem("Thống kê loại ghế",  '4-1', ),
+    ],
+  ),
+
+  GetItem(
+    "Quản lý bến bãi",
+    "5",
+    <img src="https://cdn5.vectorstock.com/i/1000x1000/96/84/bus-station-icon-in-black-bus-symbol-vector-23369684.jpg" style={{width: "25px", height: "25px"}} />,
+    [
+      GetItem("Danh sách bến bãi",  RouterConfigs.adminRouters.manageStation, ),
+      GetItem("Thống kê bến bãi",  '5-1', ),
+    ],
+  ),
+
+  GetItem(
+    "Quản lý bảng giá",
+    "6",
+    <MoneyCollectOutlined />,
+    [
+      GetItem("Danh sách bảng giá",  RouterConfigs.adminRouters.prices, ),
+      GetItem("Thống kê bảng giá",  '6-1', ),
+    ],
+  ),
+
+  GetItem(
+    "Quản lý loại giá",
+    "7",
+    <MoneyCollectOutlined />,
+    [
+      GetItem("Danh sách loại giá",  RouterConfigs.adminRouters.priceClassification, ),
+      GetItem("Thống kê loại giá",  '7-1', ),
+    ],
+  ),
+];
+
+//#endregion
+
+
+const SideBarAdmin = ({collapsed, SetCollapsed,}: { collapsed: boolean; SetCollapsed: any;}) => {
+
+  const location = useLocation();
+  const selectMenu = items2.map((item) => {
+    if (item && 'children' in item) {
+      const foundChild = item.children?.find((child) => child?.key === location.pathname);
+  
+      if (foundChild) {
+        return foundChild.key; // Trả về child.key nếu child.key === location.pathname
+      }
+    }
+  
+    return null;
+  });
+  
+  const filteredSelectMenu = selectMenu.filter((item) => item !== null);
+  console.log(selectMenu)
+  
+
+  console.log(selectMenu);
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
 
-  
-  
-  return (
-        <Sider style={{ 
-          background: colorBgContainer,
-          overflow: 'auto' ,
-          height: '100vh',
-          position: 'fixed',
-          }} width={200} trigger={null} collapsible collapsed={false}>
+  const navigate = useNavigate();
 
-        <div className="demo-logo-vertical">
-  <h2>Menu</h2>
-</div>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
-            style={{ height: "100%" }}
-            items={items2}
-            theme="light"
-          />
-        </Sider>
+  //#region Handle click menu
+const handleClick = (e: any) => {
+  console.log(e.key)
+  navigate(e.key);
+}
+//#endregion Handel click menu
+  return (
+    <Sider
+      style={{
+        background: colorBgContainer,
+        overflow: "auto",
+        height: "100vh",
+        position: "fixed",
+
+      }}
+      width={350}
+      trigger={null}
+      collapsible
+      collapsed={collapsed}
+    >
+      <div className="demo-logo-vertical">
+        {!collapsed ? (
+          <Title level={3} style={{ color: "Black", margin: 0, padding: 20 }}>
+            Menu
+          </Title>
+        ) : (
+          <Title
+            level={3}
+            style={{ color: "Black", margin: 0, padding: 30 }}
+          ></Title>
+        )}
+      </div>
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={[location.pathname]}
+        defaultOpenKeys={["sub1"]}
+        style={{ height: "100%"}}
+        items={items2}
+        theme="light"
+        onClick={(e) => {handleClick(e)}}
+      />
+    </Sider>
   );
 };
 
--- src/pages/Admin/ManageCompany/index.tsx
+++ src/pages/Admin/ManageCompany/index.tsx
@@ -1,9 +1,10 @@
 const ManageCompany = () => {
     return (
         <div>
-            <h1>ManageAccount</h1>
+            <h1>ManageCompany</h1>
+            <img src="https://th.bing.com/th/id/OIP.MpmYBjrTrzejcslqxlWcTwHaHV?rs=1&pid=ImgDetMain" />
         </div>
     );
-};
+    };
 
 export default ManageCompany;
